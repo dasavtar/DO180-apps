ddd9879fd386673832b043b36b7d68b26869b371
Student User <student@workstation.lab.example.com>
2020-03-04 09:42:41 +0000

    Changed22222 index page contents.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello,Dasavtar Golagani You are alsomost available and you are always rocking on the top please maintin thte mptot World! php version is " . PHP_VERSION . "\n";
+print "************************************************ Hello,Dasavtar Golagani You are alsomost available and you are always rocking on the top please maintin thte mptot World! php version is " . PHP_VERSION . "\n";
 ?>
